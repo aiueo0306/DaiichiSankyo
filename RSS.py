cbaaf1e8b707b94f39a513117c7ccff9d57f43ca
aiueo0306 <sato.sota@create-sd.co.jp>
2025-05-05 18:04:06 +0900

    Update RSS.py

--- RSS.py
+++ RSS.py
@@ -45,7 +45,7 @@ def extract_items(page):
         row = rows.nth(i)
         try:
             # ▼ 日付の取得と整形（例: 2025年04月24日）
-            date_text = row.locator("div.newsDate").inner_text().strip()
+            date_text = row.locator("div:nth-child(1)").inner_text().strip()
             
             import sys
             sys.exit()  # 任意の場所でスクリプトを終了
